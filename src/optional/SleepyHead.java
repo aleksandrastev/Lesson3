40c2606bf174122178031351b0881746502f9055
aleksandrastev <leafsoul91@gmail.com>
2019-05-01 10:42:46 +0200

    Complited SleepyHead exercise by using if and if/else statements

--- src/optional/SleepyHead.java
+++ src/optional/SleepyHead.java
@@ -1,22 +1,29 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 public class SleepyHead {
 
 	public static void main(String[] args) {
-		
-		boolean isWeekday, isVacation;
-		
-		/*
-		 * Ask the user for these values using
-		 * JOptionPane.showConfirmDialog(null, "is it a weekday?", "", JOptionPane.YES_NO_OPTION);
-		 */
-	
 
-		/*
-		 * Print “sleep in” if it is a vacation or a weekend. 
-		 * If it’s a weekday, print "get up lazybones!".
-		 * If it is a weekday, and we are on vacation, print “sleep in”.
-		 */
+		boolean isWeekday = true, isVacation = true;
+
+		int weekday = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "", JOptionPane.YES_NO_OPTION);
+		int vacation = JOptionPane.showConfirmDialog(null, "Are you on vacation?", "", JOptionPane.YES_NO_OPTION);
+
+		if (weekday == 1) {
+			isWeekday = false;
+		}
+
+		if (vacation == 1) {
+			isVacation = false;
+		}
+
+		if (isVacation | !isWeekday) {
+			JOptionPane.showMessageDialog(null, "Sleep in");
+		} else {
+			JOptionPane.showMessageDialog(null, "Get up lazybones!");
+
+		}
 	}
 }
-
