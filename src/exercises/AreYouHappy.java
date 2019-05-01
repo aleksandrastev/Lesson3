54d2fc4fb47caa89057cc90b45f4d997f366a540
aleksandrastev <leafsoul91@gmail.com>
2019-05-01 10:37:29 +0200

    Complited exercise for getting message by answering on question using if/else statement

--- src/exercises/AreYouHappy.java
+++ src/exercises/AreYouHappy.java
@@ -1,7 +1,23 @@
 package exercises;
 
-/* See the diagram in the "doc" directory. */
+import javax.swing.JOptionPane;
 
 public class AreYouHappy {
 
+	public static void main(String[] args) {
+
+		String answer = JOptionPane.showInputDialog("Are you happy?");
+
+		if (answer.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing.");
+		} else {
+			answer = JOptionPane.showInputDialog("Do you want to be happy?");
+			if (answer.equals("yes")) {
+				JOptionPane.showMessageDialog(null, "Change something.");
+			} else {
+				JOptionPane.showMessageDialog(null, "Keep doing whatever you're doing.");
+			}
+		}
+
+	}
 }
