b5b67ff6b4b2dda4c951ca6c9640d6486858aebc
aleksandrastev <leafsoul91@gmail.com>
2019-05-01 10:43:20 +0200

    Complited exercise by using if/else statements

--- src/optional/TheRiddler.java
+++ src/optional/TheRiddler.java
@@ -1,21 +1,52 @@
 package optional;
 
+import javax.swing.JOptionPane;
+
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
-		// 1. Make a variable to hold the score
+		int score = 0;
 
-		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
+		String answer = JOptionPane.showInputDialog(
+				"Brothers and sisters I have none, but this man's father is my father's son. \nWho is the man?");
 
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
+		if (answer.equalsIgnoreCase("my son")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score++;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong! \nThe answer is simple - the man is my son.");
+		}
 
-		// 5. Otherwise, say "wrong" and tell them the answer
+		answer = JOptionPane.showInputDialog("What can travel around the world while staying in a corner?");
 
-		// 6. Add some more riddles
+		if (answer.equalsIgnoreCase("stamp")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score++;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong! \nThe answer is simple - a stamp.");
+		}
 
-		// 2. Make a pop up to show the score.
-		
+		answer = JOptionPane.showInputDialog("What has hands but can not clap?");
+
+		if (answer.equalsIgnoreCase("clock")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score++;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong! \nThe answer is simple - a clock.");
+		}
+
+		answer = JOptionPane.showInputDialog(
+				"If you have me, you want to share me. If you share me, you haven't got me. \nWhat am I?");
+
+		if (answer.equalsIgnoreCase("secret")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score++;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong! \nThe answer is simple - a secret.");
+		}
+
+		JOptionPane.showMessageDialog(null, "Your score is " + score);
+
 	}
 }
-
