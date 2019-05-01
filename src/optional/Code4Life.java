633040d5fd1d32669653b8b07f5c202e0af64b36
aleksandrastev <leafsoul91@gmail.com>
2019-05-01 10:40:51 +0200

    Complited exercise for getting message by answering on question using if/else statement

--- src/optional/Code4Life.java
+++ src/optional/Code4Life.java
@@ -5,6 +5,7 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
 	/*
@@ -12,11 +13,11 @@ public class Code4Life {
 	 * 
 	 * 1. If it's 3 or more, tell them they're a Code Ninja.
 	 * 
-	 * 2. If it's less than 2, tell them to stop watching YouTube and write code instead.
+	 * 2. If it's less than 2, tell them to stop watching YouTube and write code
+	 * instead.
 	 * 
 	 * 3. If it's more than 5, play the Batman theme song.
 	 */
-	
 
 	private static void playBatmanTheme() {
 		try {
@@ -30,5 +31,19 @@ public class Code4Life {
 		}
 	}
 
-}
+	public static void main(String[] args) {
 
+		String answer = JOptionPane.showInputDialog("How many hours have you spent coding this week?");
+		int answerAsInt = Integer.parseInt(answer);
+
+		if (answerAsInt > 5) {
+			playBatmanTheme();
+		} else if (answerAsInt >= 3) {
+			JOptionPane.showMessageDialog(null, "You're Code Ninja!");
+		} else {
+			JOptionPane.showMessageDialog(null, "Stop watching YouTube and start writting code!");
+		}
+
+	}
+
+}
